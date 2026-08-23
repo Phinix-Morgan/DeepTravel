9d867f1b597fc641d12bfe81c1efe2c90c843eca
Phinix-Morgan <chauhanshiv2005@gmail.com>
2026-08-23 20:20:42 +0530

    feat: add booking model

---
+++ api-backend/models/Booking.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    tourPackage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "TourPackage",
+      required: true,
+    },
+
+    travelers: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+
+    pricePerPersonAtBooking: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    totalPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    status: {
+      type: String,
+      enum: [
+        "pending",
+        "confirmed",
+        "cancelled",
+        "completed",
+      ],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model(
+  "Booking",
+  bookingSchema
+);
