c7823219188cbe8d3d51f1d8068f343adb8036ee
Phinix-Morgan <chauhanshiv2005@gmail.com>
2026-08-23 20:45:27 +0530

    feat: add email verification model

---
+++ api-backend/models/EmailVerification.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const emailVerificationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+
+    tokenHash: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+emailVerificationSchema.index(
+  { expiresAt: 1 },
+  { expireAfterSeconds: 0 }
+);
+
+module.exports = mongoose.model(
+  "EmailVerification",
+  emailVerificationSchema
+);
